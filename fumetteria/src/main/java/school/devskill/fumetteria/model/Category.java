645a5a0bb2087d5a319228d966b974be45d8804f
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2022-07-27 13:25:14 +0200

    Aggiornato DB ed inserite Entity su Java

---
+++ fumetteria/src/main/java/school/devskill/fumetteria/model/Category.java
@@ -0,0 +1,22 @@
+package school.devskill.fumetteria.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    private String name;
+    @Column(nullable = false,length = 100)
+    private String surname;
+}
